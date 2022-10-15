0f7b50c66beed7e95ad0a358387c3af33a40240d
Geqianyu <17826833345@163.com>
2022-10-15 12:08:17 +0800

    修改模型

--- Rasterization/main.cpp
+++ Rasterization/main.cpp
@@ -15,7 +15,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine,
         800
     );
     Rasterization::createCamera(Eigen::Vector3d(4.6, 47.6, 75.0), Eigen::Vector3d(4.6, 47.6, 0.0), Eigen::Vector3d(0.0, 1.0, 0.0), 90.0, 1.0, 150.0);
-    Rasterization::loadObj("../obj/tails/tails.3dcool.net.obj");
+    Rasterization::loadObj("../obj/tails/tails.obj");
     Rasterization::createLight(Eigen::Vector3d(0.4, 0.4, 0.4), Eigen::Vector3d(-1.0, -1.0, -1.0), Eigen::Vector3d(0.6, 0.6, 0.6));
 
     Rasterization::show();
